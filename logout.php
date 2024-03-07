5b58e60df939c13242c973b9987cc76469ba69a3
SondosSayed <110243533+SondosSayed@users.noreply.github.com>
2024-03-08 00:19:38 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,10 @@
 <?php
- 
  session_start();
  require 'init.php';
   session_unset();
   session_destroy();
+  setcookie('user_role', 'admin', time() - 3600, '/');
+  setcookie('admin_name', $userName, time() - 3600, '/');
   header('location: login.php'); 
   die();
-
-
-
 ?>
